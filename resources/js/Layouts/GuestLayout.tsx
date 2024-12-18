72a5cfd56c8868a0cc1692acf42eadfdb9018c65
ardiman-xyz <pc.ardiman98@gmail.com>
2024-12-18 08:02:35 +0800

    feature: show application version on login page

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,8 +1,13 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { PageProps } from "@/types";
+import { Link, usePage } from "@inertiajs/react";
+import { PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
+    const props = usePage<PageProps>();
+
+    console.info(props);
+
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
@@ -14,6 +19,9 @@ export default function Guest({ children }: PropsWithChildren) {
             <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
+            <p className="text-center mt-6 text-gray-400 font-semibold text-sm">
+                {import.meta.env.VITE_LARAVEL_APP_VERSION}
+            </p>
         </div>
     );
 }
