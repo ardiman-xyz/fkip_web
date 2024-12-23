05c7405b32943965e7af1789d53f9307e391f455
ardiman-xyz <pc.ardiman98@gmail.com>
2024-12-23 08:39:38 +0800

    fixed: url stagig to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        if($this->app->environment('production')) {
+        if($this->app->environment('production') || $this->app->environment('staging')) {
             URL::forceScheme('https');
         }
 
