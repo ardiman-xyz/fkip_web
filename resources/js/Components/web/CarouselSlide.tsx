d2cb052866f23f28ae7d94783702e0e2be46aa61
ardiman-xyz <pc.ardiman98@gmail.com>
2024-12-23 16:38:08 +0800

    fixed: fixed news bugs

--- resources/js/Components/web/CarouselSlide.tsx
+++ resources/js/Components/web/CarouselSlide.tsx
@@ -1,47 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import { FeaturedNewsList } from "@/Pages/News/_types/featured-images";
-import {Slider} from "@/Pages/Slider/_types";
+import { Slider } from "@/Pages/Slider/_types";
 
 interface HeroSectionProps {
     featuredNews?: FeaturedNewsList;
     defaultSliders: Slider[];
 }
 
-export  const CarouselSlide = ({ slide, type }: {
-    slide: any,
-    type: 'featured' | 'default'
+export const CarouselSlide = ({
+    slide,
+    type,
+}: {
+    slide: any;
+    type: "featured" | "default";
 }) => {
     const [isLoaded, setIsLoaded] = useState(false);
 
     const ImageContent = () => (
         <>
             <img
-                src={type === 'featured' ? slide.slider_image.paths.blur : slide.media.paths.blur}
+                src={
+                    type === "featured"
+                        ? slide.slider_image.path
+                        : slide.media.paths.blur
+                }
                 alt="blur"
                 className={`w-full md:h-[694px] h-[400px] object-cover transition-opacity duration-500 ${
-                    isLoaded ? 'opacity-0' : 'opacity-100'
+                    isLoaded ? "opacity-0" : "opacity-100"
                 }`}
             />
 
             <img
-                src={type === 'featured' ? slide.slider_image.paths.original : slide.media.paths.original}
-                alt={type === 'featured' ? slide.translations.id.title : slide.media.file_name}
+                src={
+                    type === "featured"
+                        ? slide.slider_image.path
+                        : slide.media.paths.original
+                }
+                alt={
+                    type === "featured"
+                        ? slide.translations.id.title
+                        : slide.media.file_name
+                }
                 className={`w-full md:h-[694px] h-[400px] object-cover absolute top-0 left-0 transition-opacity duration-500 ${
-                    isLoaded ? 'opacity-100' : 'opacity-0'
+                    isLoaded ? "opacity-100" : "opacity-0"
                 }`}
                 onLoad={() => setIsLoaded(true)}
             />
 
-            {type === 'featured' && (
+            {type === "featured" && (
                 <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
                     <div className="container mx-auto px-4 text-center">
-                        <h2 className="text-white md:text-4xl text-2xl font-bold mb-4">
-                            {slide.translations.id.title}
-                        </h2>
-                        <a href={`/berita/${slide.translations.id.slug}`}
+                        <a
+                            href={`/berita/${slide.translations.id.slug}`}
                             className="bg-white text-green-600 md:px-6 px-4 md:py-3 py-2 rounded-sm font-semibold hover:bg-green-100 inline-block transition duration-300"
-                            >
+                        >
                             Baca Selengkapnya
                         </a>
                     </div>
@@ -50,7 +63,7 @@ export  const CarouselSlide = ({ slide, type }: {
         </>
     );
 
-    if (type === 'default' && slide.url) {
+    if (type === "default" && slide.url) {
         return (
             <a
                 href={slide.url}
@@ -66,5 +79,5 @@ export  const CarouselSlide = ({ slide, type }: {
         <div className="relative">
             <ImageContent />
         </div>
- );
+    );
 };
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,28 +1,35 @@
 import Guest2 from "@/Layouts/GuestLayout2";
 import { Head } from "@inertiajs/react";
-import {News} from "@/Pages/News/_types";
-import {Event} from "@/Pages/Event/_types";
-import { NewsSection} from "@/Components/web/News";
-import {EventSection} from "@/Components/web/EventSection";
-import { FeaturesSection} from "@/Components/web/Features";
+import { News } from "@/Pages/News/_types";
+import { Event } from "@/Pages/Event/_types";
+import { NewsSection } from "@/Components/web/News";
+import { EventSection } from "@/Components/web/EventSection";
+import { FeaturesSection } from "@/Components/web/Features";
 import HeroSection from "@/Components/web/HeroSection";
 import VideoSection from "@/Components/web/VideoSection";
-import {FeaturedNewsList} from "@/Pages/News/_types/featured-images";
-import {Slider} from "@/Pages/Slider/_types";
+import { FeaturedNewsList } from "@/Pages/News/_types/featured-images";
+import { Slider } from "@/Pages/Slider/_types";
 
 interface WelcomeProps {
-    news : News[]
-    events: Event[]
+    news: News[];
+    events: Event[];
     featuredNews: FeaturedNewsList;
-    defaultSliders: Slider[]
+    defaultSliders: Slider[];
 }
 
-export default function Welcome({news, events, featuredNews, defaultSliders}:  WelcomeProps) {
-
+export default function Welcome({
+    news,
+    events,
+    featuredNews,
+    defaultSliders,
+}: WelcomeProps) {
     return (
         <Guest2>
             <Head title="Home" />
-            <HeroSection featuredNews={featuredNews} defaultSliders={defaultSliders} />
+            <HeroSection
+                featuredNews={featuredNews}
+                defaultSliders={defaultSliders}
+            />
             <FeaturesSection />
             <NewsSection news={news} />
             <EventSection events={events} />
